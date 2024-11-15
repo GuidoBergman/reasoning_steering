1880d01b26c73065d8857f194902923e7b9a8c30
GuidoBergman <74883160+GuidoBergman@users.noreply.github.com>
2024-11-15 17:05:57 -0300

    fix contrastive pairs

--- dataset/contrastive_pairs.py
+++ dataset/contrastive_pairs.py
@@ -2,7 +2,7 @@ from dataset.load_dataset import load_dataset
 
 def get_contrastive_pairs(train_correct_ids, train_incorrect_ids, solver):    
 
-    train_correct = load_dataset(train_correct_ids)
-    train_incorrect = load_dataset(train_incorrect_ids)
+    train_correct = load_dataset(train_correct_ids, solver)
+    train_incorrect = load_dataset(train_incorrect_ids, solver)
 
     return train_correct, train_incorrect
