3d502f65b4c4859a67d580e7f5563e2402de5502
Guido Bergman <gbergman@tdi.com.ar>
2024-11-23 21:27:31 -0300

    Delete unused files

--- dataset/contrastive_pairs.py
+++
@@ -1,8 +0,0 @@
-from dataset.load_dataset import load_dataset
-
-def get_contrastive_pairs(train_correct_ids, train_incorrect_ids, solver):    
-
-    train_correct = load_dataset(train_correct_ids, solver)
-    train_incorrect = load_dataset(train_incorrect_ids, solver)
-
-    return train_correct, train_incorrect
